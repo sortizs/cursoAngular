e28114e75b33286bdf85cb8cb6e0f383f15a6394
sortizs <sortizs@outlook.com>
2019-11-18 10:57:26 -0500

    Forms data approach

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, MinLengthValidator } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -8,40 +9,112 @@ import { FormGroup, FormControl, Validators, MinLengthValidator } from '@angular
 })
 export class DataComponent {
 
-  form: FormGroup;
+  userForm: FormGroup;
 
-  user: object = {
+  user: any = {
     fullName: {
-      name: '',
-      lastName: ''
+      name: 'Sebastian',
+      lastName: 'Ortiz Serna'
     },
-    email: ''
+    email: 'sebastian.ortiz@uruit.com',
+    hobbies: ['Run'],
+    username: '',
+    password: '',
+    password2: ''
   };
 
   constructor() {
-    console.log(this.user);
-    this.form = new FormGroup({
+    // console.log(this.user);
+    this.userForm = new FormGroup({
       fullName: new FormGroup({
         name: new FormControl(
           '',
-          [Validators.required,
+          [
+            Validators.required,
             Validators.minLength(3)
           ]
         ),
-        lastName: new FormControl('', Validators.required)}
+        lastName: new FormControl(
+          '',
+          Validators.required
+        )
+      }
       ),
       email: new FormControl(
         '',
-        [Validators.required,
-        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
-        ])
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
+        ]
+      ),
+      hobbies: new FormArray(
+        [
+          new FormControl('', Validators.required)
+        ]
+      ),
+      username: new FormControl(
+        '',
+        Validators.required,
+        this.userExists
+      ),
+      password: new FormControl(
+        '',
+        Validators.required
+      ),
+      password2: new FormControl()
     });
+    // this.userForm.setValue(this.user);
+    this.userForm.controls['password2'].setValidators(
+      [
+        Validators.required,
+        this.noIgual.bind(this.userForm)
+      ]
+    )
+  }
 
+  addHobbie() {
+    (<FormArray>this.userForm.controls['hobbies']).push(
+      new FormControl('', Validators.required)
+    );
+  }
+
+  userExists(control: FormControl): Promise<any> | Observable<any> {
+    let promise = new Promise(
+      (resolve) => {
+        setTimeout(() => {
+          if (control.value.toLowerCase() === "sortizs") {
+            resolve({ exists: true });
+          } else {
+            resolve(null);
+          }
+        }, 2000);
+      }
+    )
+
+    return promise;
+  }
+
+  noIgual(control: FormControl): { [s: string]: boolean } {
+
+    let form: any = this;
+    if (control.value !== form.controls['password'].value) {
+      return {
+        noiguales: true
+      }
+    }
+    return null;
   }
 
   saveChanges() {
-    console.log(this.form.value);
-    console.log(this.form);
+    console.log(this.userForm);
+
+    this.userForm.reset({
+      fullName: {
+        name: '',
+        lastName: ''
+      },
+      email: ''
+    });
   }
 
 }
